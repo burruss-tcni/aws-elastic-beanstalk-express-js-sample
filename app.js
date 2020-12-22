6b63e94f4db8c53b98138fbef215080e16a970cb
burruss-tcni <burruss@tcni.net>
2020-12-22 08:18:18 -0500

    changed msg - 2nd time

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Today is a day the Lord has made. Let us rejoice and be glad in it!'));
+app.get('/', (req, res) => res.send('This is a day the Lord has made. Let us rejoice and be glad in it!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
