42492e7cb8803d387176722d0ff7e13d81f0d0ad
burruss-tcni <burruss@tcni.net>
2020-12-23 09:02:10 -0500

    3rd change to statement - testing cp manual approval

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This is a day the Lord has made. Let us rejoice and be glad in it!'));
+app.get('/', (req, res) => res.send('Proverbs 2:3: Yeah, if the criest after knowledge, and liftest up thy voice for understanding;'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
