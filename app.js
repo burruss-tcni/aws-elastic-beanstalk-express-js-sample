d1eb52aa8ca6ec5af3ef76d8e0d01ed110d64508
burruss-tcni <burruss@tcni.net>
2020-12-23 09:26:09 -0500

    changed line 5 statement - full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Proverbs 2:3: Yeah, if the criest after knowledge, and liftest up thy voice for understanding;'));
+app.get('/', (req, res) => res.send('Life is good!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
