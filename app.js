008534dbd6eabbd2cf871c94aa37f4ee5b817290
burruss-tcni <73897510+burruss-tcni@users.noreply.github.com>
2021-01-20 09:12:00 -0500

    Update app.js
    changed app.get line 5 - Joe Biden's Inauguration Day!

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('The holidays are here!'));
+app.get('/', (req, res) => res.send('Today is Inauguration Day, Jan 20, 2021'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
