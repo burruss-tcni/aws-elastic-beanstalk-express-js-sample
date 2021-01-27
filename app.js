91300d5ceea3e1c7a88ceeb0003e1714a01dade3
burruss-tcni <73897510+burruss-tcni@users.noreply.github.com>
2021-01-27 15:30:30 -0500

    Update app.js
    added window popup prompt asking for your name

--- app.js
+++ app.js
@@ -4,5 +4,8 @@ const port = 8080;
 
 app.get('/', (req, res) => res.send('Hello Team!'));
 
+var name = window.prompt("Enter your name: ");
+alert("Your name is " + name);
+
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
